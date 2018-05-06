10aa315c724ef14a734cc763d0d2338a6a00c55b
Piotr Slawek <pslawek@cisco.com>
2018-05-06 11:46:25 +0200

    Poprawiona indentacja w kodzie

--- firebase-auth/public/app.js
+++ firebase-auth/public/app.js
@@ -48,29 +48,36 @@ function handleSignUp() {
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
     if (email.length < 4) {
-    alert('Please enter an email address.');
-    return;
+        alert('Please enter an email address.');
+        return;
     }
     if (password.length < 4) {
-    alert('Please enter a password.');
-    return;
+        alert('Please enter a password.');
+        return;
     }
     // Sign in with email and pass.
     // [START createwithemail]
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // [START_EXCLUDE]
-    if (errorCode == 'auth/weak-password') {
-      alert('The password is too weak.');
-    } else {
-      alert(errorMessage);
-    }
-    console.log(error);
-    // [END_EXCLUDE]
-    });
-    // [END createwithemail]
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(function(result) {
+            alert("Account created");
+        })
+        .catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // [START_EXCLUDE]
+            if (error == null) {
+                alert("Account created");
+            }
+            if (errorCode == 'auth/weak-password') {
+              alert('The password is too weak.');
+            } else {
+              alert(errorMessage);
+            }
+            console.log(error);
+            // [END_EXCLUDE]
+            });
+        // [END createwithemail]
 }
 
 function sendEmailVerification() {
