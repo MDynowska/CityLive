7234bab4b43dc829645ac347375cc6974e33e6eb
pisxx <piotrek.slawek@gmail.com>
2018-05-10 10:13:27 +0200

    Dodany kod JavaScript dla Menu oraz integracji z firebase DB

---
+++ apk/www/js/buttons.js
@@ -0,0 +1,134 @@
+var userId = localStorage.getItem('userId')
+var userEmail = localStorage.getItem('userEmail')
+var userFavorites = localStorage.getItem('userFavorites')
+
+
+function initApp() {
+    var config = {
+        apiKey: "AIzaSyDtrie9w23IwHBTjv6RsJdOYptna740fY8",
+        authDomain: "fir-tests-504a2.firebaseapp.com",
+        databaseURL: "https://fir-tests-504a2.firebaseio.com",
+        projectId: "fir-tests-504a2",
+        storageBucket: "fir-tests-504a2.appspot.com",
+        messagingSenderId: "440215787226"
+    }
+    firebase.initializeApp(config);
+}
+
+
+function singOut() {
+    if (!firebase.apps.length) {
+        initApp();
+    }
+    firebase.auth().signOut()
+        .then(window.location.href = 'index.html');
+}
+
+function myProfile(userId) {
+    if (!firebase.apps.length) {
+        initApp();
+    }
+    // var userId = firebase.auth().getUid();
+    // console.log(userLogged);
+    var user = firebase.database().ref('users/' + userId);
+    user.once('value', function(snapshot) {
+        if (!snapshot.val().favorites) {
+            alert('You do not have any favorites');
+        } else {
+            alert(snapshot.val().favorites);
+        }
+        // snapshot.forEach(function(childSnapshot) {
+        //     if (childSnapshot.val().username === userLogged) {
+        //         console.log(childSnapshot.val().favorites);
+        //         };
+        //     /* console.log(childSnapshot.val().username); */
+        //     });
+        });
+}
+
+function getCategories() {
+    if (!firebase.apps.length) {
+        initApp();
+    }
+    // var userLogged = firebase.auth().getUid();
+    // console.log(userLogged);
+    var categories = firebase.database().ref('categories');
+    categories.once('value', function(snapshot) {
+        console.log(snapshot.val());
+        alert(snapshot.val())
+        // snapshot.forEach(function(childSnapshot) {
+        //     if (childSnapshot.val().username === userLogged) {
+        //         console.log(childSnapshot.val().favorites);
+        //         };
+        // console.log(childSnapshot.val())
+        // alert()
+        //     });
+        });
+    // });
+}
+
+function writeUserData(userId, email, favorites) {
+  firebase.database().ref('users/' + userId).set({
+    email: email,
+    favorites: favorites,
+  });
+}
+
+
+function editFavorites() {
+    if (!firebase.apps.length) {
+        initApp();
+    }
+    // var userId = localStorage.getItem('userId')
+    var userId = userId;
+    var email = userEmail;
+    var favorites = userFavorites;
+    // var email = userEmail;
+    // if (!userId) {
+    //     setTimeout(function() {
+    //         firebase.auth().getUid();
+    //     }, 100);
+    // }
+    writeUserData(userId, email, favorites);
+}
+
+// function searchAttractions() {
+//     window.location.href = 'searchAttractions.html';
+// }
+
+$( document ).ready(function() {
+    $( ".cross" ).hide();
+    $( ".menu" ).hide();
+    $( ".hamburger" ).click(function() {
+        $( ".menu" ).slideToggle( "slow", function() {
+        $( ".hamburger" ).hide();
+        $( ".cross" ).show();
+        });
+    });
+
+    $( ".cross" ).click(function() {
+        $( ".menu" ).slideToggle( "slow", function() {
+            $( ".cross" ).hide();
+            $( ".hamburger" ).show();
+        });
+    });
+    $( "#myProfile" ).click(function() {
+        myProfile(userId);
+    });
+    $( "#getCategories" ).click(function() {
+        getCategories();
+    });
+    $( "#logOut" ).click(function() {
+        singOut();
+        // toggleSignIn();
+        // initApp();
+        // firebase.auth().signOut();
+    });
+    // $( "#searchAttractionsMenu" ).click(function() {
+    //     window.location.href = 'searchAttractions.html';
+    //     // searchAttractions();
+    // });
+});
+// $( "#editFavorites" ).click(function() {
+//     editFavorites(userId);
+// });
---
+++ apk/www/js/favorites.js
@@ -0,0 +1,217 @@
+// array.indexOf(newItem) === -1 ? array.push(newItem) : console.log("This item already exists");
+var favorites = [];
+var userId = localStorage.getItem('userId')
+var userEmail = localStorage.getItem('userEmail')
+var userFavorites = localStorage.getItem('userFavorites')
+
+function checkUncheck(box) {
+    // console.log(favorites);
+    if($(box).is(':checked')) {
+        console.log(box['name'] + " Checked");
+        favorites.indexOf(box['name']) === -1 ? favorites.push(box['name']) : console.log("This item already exists");
+        // favorites.push(box['name']);
+        // console.log(favorites);
+    } else {
+        console.log(box['name'] + " Unchecked");
+        var favoriteIndex = favorites.indexOf(box['name']);
+        // console.log(favoriteIndex);
+        if (favoriteIndex < 1) {
+            favorites.splice(favoriteIndex, 1);
+        } else {
+            console.log("This item is not in favorites");
+        }
+        // console.log(favorites);
+        // favorites.indexOf(box['name']) != -1 ? favorites.push(box['name']) : console.log("This item already exists");
+        // Checkbox is not checked..
+    }
+    console.log(favorites);
+}
+//
+// function check(box) {
+//     if($(box.checked)) {
+//         console.log(box['name'] + " Checked");
+//         favorites.indexOf(box['name']) === -1 ? favorites.push(box['name']) : console.log("This item already exists");
+//         // favorites.push(box['name']);
+//         console.log(favorites);
+//     } else {
+//         console.log(box['name'] + " Unchecked");
+//         var favoriteIndex = favorites.indexOf(box['name']);
+//         if (favoriteIndex < 1) {
+//             favorites.splice(favoriteIndex, 1);
+//         } else {
+//             console.log("This item is not in favorites");
+//         }
+//         console.log(favorites);
+//         favorites.indexOf(box['name']) != -1 ? favorites.push(box['name']) : console.log("This item already exists");
+//         // Checkbox is not checked..
+//     }
+// }
+//
+// function uncheck(box) {
+//     if($(!box.checked)) {
+//         console.log(box['name'] + " Unchecked");
+//         var favoriteIndex = favorites.indexOf(box['name']);
+//         if (favoriteIndex < 1) {
+//             favorites.splice(favoriteIndex, 1);
+//         } else {
+//             console.log("This item is not in favorites");
+//         }
+//         console.log(favorites);
+//         favorites.indexOf(box['name']) != -1 ? favorites.push(box['name']) : console.log("This item already exists");
+//     }
+// }
+//
+$( document ).ready(function() {
+    $('input').change(function() {
+        var checkboxName = this['name'];
+        switch(checkboxName) {
+            case 'cinemas':
+                // console.log(checkboxName);
+                checkUncheck(this);
+                // check(this);
+                // uncheck(this);
+                break;
+            case 'museums':
+                checkUncheck(this);
+                break;
+            case 'spa':
+                checkUncheck(this);
+                break;
+            case 'restaurant':
+                checkUncheck(this);
+                break;
+            case 'gym':
+                checkUncheck(this);
+                break;
+            case 'zoo':
+                checkUncheck(this);
+                break;
+            case 'cafe':
+                checkUncheck(this);
+                break;
+            case 'art':
+                checkUncheck(this);
+                break;
+        }
+    })
+});
+
+function initApp() {
+    var config = {
+        apiKey: "AIzaSyDtrie9w23IwHBTjv6RsJdOYptna740fY8",
+        authDomain: "fir-tests-504a2.firebaseapp.com",
+        databaseURL: "https://fir-tests-504a2.firebaseio.com",
+        projectId: "fir-tests-504a2",
+        storageBucket: "fir-tests-504a2.appspot.com",
+        messagingSenderId: "440215787226"
+    }
+    firebase.initializeApp(config);
+}
+
+function editFavorites(userId, email, userFavorites) {
+    if (!firebase.apps.length) {
+        initApp();
+    }
+    // var userId = localStorage.getItem('userId')
+    // var userId = userId;
+    // var email = userEmail;
+    // var favorites = userFavorites;
+    // var email = userEmail;
+    // if (!userId) {
+    //     setTimeout(function() {
+    //         firebase.auth().getUid();
+    //     }, 100);
+    // }
+    writeUserData(userId, email, favorites);
+}
+
+function saveFavorites() {
+    localStorage.setItem('userFavorites', favorites);
+    console.log(favorites);
+    editFavorites(userId, userEmail, userFavorites);
+}
+
+// function saveFavorites() {
+//     var favorites = [];
+//     // if($('input[cinemas]').is(':checked')) {
+//     if (document.getElementById("cinemas").checked = true) {
+//         // console.log(cinemas);
+//         favorites.push("cinemas")
+//     } else {
+//         var favoriteIndex = favorites.indexOf("cinemas");
+//         if (favoriteIndex < 1) {
+//             favorites.splice(favoriteIndex, 1);
+//         }
+//     }
+    // // if($('input[museums]').is(':checked')) {
+    // if (document.getElementById("museums").checked = true) {
+    //     // console.log(cinemas);
+    //     favorites.push("museums")
+    // } else {
+    //     var favoriteIndex = favorites.indexOf("museums");
+    //     if (favoriteIndex < 1) {
+    //         favorites.splice(favoriteIndex, 1);
+    //     }
+    // }
+    // if($('input[spa]').is(':checked')) {
+    // // if (document.getElementsByName("spa").checked = true) {
+    //     // console.log(cinemas);
+    //     favorites.push("spa")
+    // } else {
+    //     var favoriteIndex = favorites.indexOf("spa");
+    //     if (favoriteIndex < 1) {
+    //         favorites.splice(favoriteIndex, 1);
+    //     }
+    // }
+    // if($('input[restaurant]').is(':checked')) {
+    // // if (document.getElementsByName("restaurant").checked = true) {
+    //     // console.log(cinemas);
+    //     favorites.push("restaurant")
+    // } else {
+    //     var favoriteIndex = favorites.indexOf("restaurant");
+    //     if (favoriteIndex < 1) {
+    //         favorites.splice(favoriteIndex, 1);
+    //     }
+    // }
+    // if($('input[gym]').is(':checked')) {
+    // // if (document.getElementsByName("gym").checked = true) {
+    //     // console.log(cinemas);
+    //     favorites.push("gym")
+    // } else {
+    //     var favoriteIndex = favorites.indexOf("gym");
+    //     if (favoriteIndex < 1) {
+    //         favorites.splice(favoriteIndex, 1);
+    //     }
+    // }
+    // if($('input[zoo]').is(':checked')) {
+    // // if (document.getElementsByName("zoo").checked = true) {
+    //     // console.log(cinemas);
+    //     favorites.push("zoo")
+    // } else {
+    //     var favoriteIndex = favorites.indexOf("zoo");
+    //     if (favoriteIndex < 1) {
+    //         favorites.splice(favoriteIndex, 1);
+    //     }
+    // }
+    // if($('input[cafe]').is(':checked')) {
+    // // if (document.getElementsByName("cafe").checked = true) {
+    //     // console.log(cinemas);
+    //     favorites.push("cafe")
+    // } else {
+    //     var favoriteIndex = favorites.indexOf("cafe");
+    //     if (favoriteIndex < 1) {
+    //         favorites.splice(favoriteIndex, 1);
+    //     }
+    // }
+    // if($('input[art]').is(':checked')) {
+    // // if (document.getElementsByName("art").checked = true) {
+    //     // console.log(cinemas);
+    //     favorites.push("art")
+    // } else {
+    //     var favoriteIndex = favorites.indexOf("art");
+    //     if (favoriteIndex < 1) {
+    //         favorites.splice(favoriteIndex, 1);
+    //     }
+    // }
+//     console.log(favorites);
+// }
